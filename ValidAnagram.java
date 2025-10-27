ff7871e8aba44e2f97693b8d76949187bd562ed6
Siddharth-iang <siddureddyvr1115@gmail.com>
2025-10-27 11:57:26 +0530

    Implement isAnagram method in ValidAnagram.java

---
+++ ValidAnagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        HashMap <Character,Integer> freqMap = new HashMap<>();
+
+        for(char c: s.toCharArray()){
+            freqMap.put(c, freqMap.getOrDefault(c,0) + 1);
+        };
+
+        for(char c: t.toCharArray()){
+            if(!freqMap.containsKey(c)){
+                return false;
+            }
+
+            freqMap.put(c, freqMap.get(c)-1);
+            if(freqMap.get(c) < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
